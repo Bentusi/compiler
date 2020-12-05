c96a77b48714ec7f0ba5045903f511e0c6a55015
JIANG Wei <jiangwei.chn@gmail.com>
2020-12-05 22:47:00 +0800

    实现WHILE

--- if/main.cpp
+++ if/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include "statement.hpp"
 #include "env.hpp"
@@ -12,18 +11,13 @@ int main(int argc, char const *argv[])
 
     expr* a = new add(new num(1), new num(2));
     expr* b = new mul(new num(2), new num(3));
-    expr* l = new lt(a, b);
     env*  e = new env();
-    // e->set("p", new num(2));
-    // e->set("q", new num(3));
-    
-    expr* p = new var("p");
-    expr* q = new var("q");
+    e->set("x", new num(0));
 
-    expr* m = new assign("x", a);
-    expr* n = new assign("x", b);
-    expr* k = new IF(l, m, n);
-    vm->run(k, e);
+    expr* c = new lt(new var("x"), new num(3));
+    expr* q = new add(new var("x"), new num(1));
+
+    vm->run(new WHILE(c, new assign("x", q)), e);
 
     return 0;
 }
--- if/statement.hpp
+++ if/statement.hpp
@@ -194,12 +194,112 @@ bool IF::reduceable()
 
 void IF::to_s()
 {
-    cout << "IF (";
+    cout << "IF ";
     m_con->to_s();
-    cout << ") THEN ";
+    cout << " THEN ";
     m_t->to_s();
     cout << " ELSE ";
     m_f->to_s();
+    cout << " END";
+}
+
+
+/**********************************************************/
+/**/
+/**********************************************************/
+class SEQ : public expr
+{
+private:
+    expr* m_f;
+    expr* m_s;
+public:
+    SEQ(expr* f, expr* s);
+    ~SEQ();
+
+    expr* reduce(env* e);
+    bool  reduceable();
+    void  to_s();
+};
+
+SEQ::SEQ(expr* f, expr* s)
+{
+    m_f = f;
+    m_s = s;
+}
+
+SEQ::~SEQ()
+{
+}
+
+expr* SEQ::reduce(env* e)
+{
+    if (m_f->reduceable())
+    {
+        return new SEQ(m_f->reduce(e), m_s);
+    }
+    else
+    {
+        return m_s;
+    }
+}
+
+bool SEQ::reduceable()
+{
+    return true;
+}
+
+void SEQ::to_s()
+{
+    cout << "(";
+    m_f->to_s();
+    cout << "; ";
+    m_s->to_s();
+    cout << ")";
+}
+
+/**********************************************************/
+/**/
+/**********************************************************/
+class WHILE : public expr
+{
+private:
+    expr* m_con;
+    expr* m_t;
+public:
+    WHILE(expr* con, expr* t);
+    ~WHILE();
+
+    expr* reduce(env* e);
+    bool  reduceable();
+    void  to_s();
+};
+
+WHILE::WHILE(expr* con, expr* t)
+{
+    m_con = con;
+    m_t = t;
+}
+
+WHILE::~WHILE()
+{
+}
+
+expr* WHILE::reduce(env* e)
+{
+    return new IF(m_con->reduce(e), new SEQ(m_t, this), new donothing());
+}
+
+bool WHILE::reduceable()
+{
+    return true;
+}
+
+void WHILE::to_s()
+{
+    cout << "WHILE ";
+    m_con->to_s();
+    cout << " THEN ";
+    m_t->to_s();
     cout << " END";
 }
 
